f56b3fdcf16ebeed70fc4571e6549351097d9bf0
Vikash Kumar <35370115+vikash-kumar01@users.noreply.github.com>
2022-01-04 17:07:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region  = var.region
+}
+
+
+resource "aws_s3_bucket" "b" {
+  bucket = "myawsbucket123455545"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
